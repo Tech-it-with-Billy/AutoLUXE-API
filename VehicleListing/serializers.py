d440cce5521b75a5e4f12e30a30fd382cf2e06ac
Billy Och <ochiengbilly02@gmail.com>
2026-02-07 02:24:14 +0300

    Adding serializers, views and urls files to mange vehicle data handling and routing

---
+++ VehicleListing/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Vehicles, VehicleImages
+
+class VehicleSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True
+    )
+    
+    class Meta:
+        model = Vehicles
+        fields = [
+            'id', 'make', 'models', 'body_type', 'year', 
+            'cost', 'engine', 'transmission', 'seats', 'gas_type'
+        ]
+        
+        def create(self, validated_data):
+            images = validated_data.pop('images')
+            vehicle = Vehicles.objects.create(**validated_data)
+            
+            for image in images:
+                VehicleImages.objects.create(
+                    vehicle_id = vehicle,
+                    image = image
+                )
+            
+            return vehicle
--- VehicleListing/views.py
+++ VehicleListing/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Vehicles
+from .serializers import VehicleSerializer
+
+from rest_framework import generics
+from rest_framework.parsers import MultiPartParser, FormParser
 
 # Create your views here.
+class CreateVehicleView(generics.CreateAPIView):
+    queryset = Vehicles.objects.all()
+    serializer_class = VehicleSerializer
+    parser_classes = [MultiPartParser, FormParser]
+
+class VehicleListView(generics.ListAPIView):
+    queryset = Vehicles.objects.all()
+    serializer_class = VehicleSerializer
+    
